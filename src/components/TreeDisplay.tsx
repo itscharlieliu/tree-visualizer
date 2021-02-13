eea0b9c903decea4afd11af63696beef8d444266
Charlie <itscharlieliu@gmail.com>
2021-02-12 16:51:09 -0800

    Create display using canvas

---
+++ src/components/TreeDisplay.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TreeDisplay = (): JSX.Element => {
+    return (
+        <canvas id="test" width="150" height="150">
+            This browser is not supported.
+        </canvas>
+    );
+};
+
+export default TreeDisplay;
