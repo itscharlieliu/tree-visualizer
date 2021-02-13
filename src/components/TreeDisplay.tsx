a8508bd6042a38671e3db8ba9968cc3ed4eb1dd4
Charlie <itscharlieliu@gmail.com>
2021-02-13 14:18:16 -0800

    Use canvas to draw rectangle

--- src/components/TreeDisplay.tsx
+++ src/components/TreeDisplay.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import useMount from "../utils/useMount";
 
 const TreeDisplay = (): JSX.Element => {
     // TODO Follow this tutorial https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
-    return (
-        <canvas id="test" width="150" height="150">
-            This browser is not supported.
-        </canvas>
-    );
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useMount(() => {
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext("2d");
+
+            if (context) {
+                context.fillStyle = "#000000";
+                context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+            }
+        }
+    });
+
+    return <canvas ref={canvasRef}>This browser is not supported.</canvas>;
 };
 
 export default TreeDisplay;
