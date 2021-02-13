ef3b1f7a8a29a1f80fa748b2faa692d3136b9e00
Charlie <itscharlieliu@gmail.com>
2021-02-12 16:51:34 -0800

    Added link to tutorial

--- src/components/TreeDisplay.tsx
+++ src/components/TreeDisplay.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const TreeDisplay = (): JSX.Element => {
+    // TODO Follow this tutorial https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
     return (
         <canvas id="test" width="150" height="150">
             This browser is not supported.
