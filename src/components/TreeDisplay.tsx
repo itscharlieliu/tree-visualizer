17c8c6f12ef7d228a125a785d4119bcfeaa15da6
Charlie <itscharlieliu@gmail.com>
2021-02-14 11:55:58 -0800

    Add button to add new treenode

--- src/components/TreeDisplay.tsx
+++ src/components/TreeDisplay.tsx
@@ -1,7 +1,14 @@
-import React, { Context, useRef } from "react";
+import React, { useRef } from "react";
 import useMount from "../utils/useMount";
+import Tree from "../data_structures/tree";
 
-const drawTree = (context: CanvasRenderingContext2D) => {
+interface TreeNode {
+    val: number;
+    left?: TreeNode;
+    right?: TreeNode;
+}
+
+const drawTree = (tree: Tree<number>, context: CanvasRenderingContext2D) => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.fillStyle = "#000000";
     context.beginPath();
@@ -11,22 +18,39 @@ const drawTree = (context: CanvasRenderingContext2D) => {
 };
 
 const TreeDisplay = (): JSX.Element => {
-    // TODO Follow this tutorial https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
+    const treeRef = useRef<Tree<number>>(new Tree<number>());
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useMount(() => {
+    const renderCanvas = () => {
         if (canvasRef.current) {
             const canvas = canvasRef.current;
             const context = canvas.getContext("2d");
 
             if (context) {
-                drawTree(context);
+                drawTree(treeRef.current, context);
             }
         }
+    };
+
+    useMount(() => {
+        renderCanvas();
     });
 
-    return <canvas ref={canvasRef}>This browser is not supported.</canvas>;
+    const handleAddNumber = () => {
+        // temp random number
+        const num = Math.floor(Math.random() * 10);
+
+        treeRef.current.set(num, num);
+        renderCanvas();
+    };
+
+    return (
+        <div>
+            <button onClick={handleAddNumber}>add random number</button>
+            <canvas ref={canvasRef}>This browser is not supported.</canvas>
+        </div>
+    );
 };
 
 export default TreeDisplay;
