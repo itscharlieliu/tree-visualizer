2f06748bc9543e5da551e7e3319195726dea6b73
Charlie <itscharlieliu@gmail.com>
2021-02-14 11:13:16 -0800

    Start drawing tree structure on canvas

--- src/components/TreeDisplay.tsx
+++ src/components/TreeDisplay.tsx
@@ -1,5 +1,14 @@
-import React, { useRef } from "react";
+import React, { Context, useRef } from "react";
 import useMount from "../utils/useMount";
+
+const drawTree = (context: CanvasRenderingContext2D) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "#000000";
+    context.beginPath();
+    context.arc(50, 100, 20, 0, 2 * Math.PI);
+    // context.fill();
+    context.stroke();
+};
 
 const TreeDisplay = (): JSX.Element => {
     // TODO Follow this tutorial https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
@@ -12,8 +21,7 @@ const TreeDisplay = (): JSX.Element => {
             const context = canvas.getContext("2d");
 
             if (context) {
-                context.fillStyle = "#000000";
-                context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+                drawTree(context);
             }
         }
     });
